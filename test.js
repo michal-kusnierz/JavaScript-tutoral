e62c980faca081934d71f869e266f5524a8ea80c
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-25 15:10:47 +0200

    some math functions

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-function getAverage (a,b){
+var a = 5;
+var b = 5;
 
-    var average = (a + b) / 2;      // local variable
-    console.log(average);
-    return average;
+console.log(a + b);
+console.log(typeof (a + b) );
 
-}
-
-var myResult = getAverage(7,12);    // global variable
-
-
-function logResult(){
-    console.log("the average is " + myResult + " inside the function");
-}
-
-logResult();
+// some math functions
+console.log(Math.round(7.8));
+console.log(Math.floor(7.8));
+console.log(Math.ceil(7.8));
+console.log(Math.max(7, 4, 9, 8));
+console.log(Math.PI);
