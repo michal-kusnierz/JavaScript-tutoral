4889477fec5db93c7dd81aed41f0e3b1bb89aafb
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-25 15:02:56 +0200

    function example

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 function getAverage (a,b){
 
-    var average = (a + b) / 2;
+    var average = (a + b) / 2;      // local variable
     console.log(average);
     return average;
 
 }
 
-var myResult = getAverage(7,12); 
-console.log("the average is " + myResult);
+var myResult = getAverage(7,12);    // global variable
+
+
+function logResult(){
+    console.log("the average is " + myResult + " inside the function");
+}
+
+logResult();
