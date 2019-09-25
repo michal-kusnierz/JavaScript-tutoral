97ede3f709d936e124b2339439bc81395fd1cfaf
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-25 15:27:05 +0200

    isNaN

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-var a = 5;
+var a = "apple";
 var b = 5;
 
-console.log(a + b);
-console.log(typeof (a + b) );
+if (isNaN(a)){
 
-// some math functions
-console.log(Math.round(7.8));
-console.log(Math.floor(7.8));
-console.log(Math.ceil(7.8));
-console.log(Math.max(7, 4, 9, 8));
-console.log(Math.PI);
+    console.log("that aint even a number, thickie");
+
+} else{
+
+    console.log("meaning of life is " + (a * b));
+
+}
