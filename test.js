e2822cef30ec2924be00fced3d378709bc048d88
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-25 14:41:38 +0200

    return

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ function getAverage (a,b){
 
     var average = (a + b) / 2;
     console.log(average);
+    return average;
 
 }
 
-getAverage(7,12); 
+var myResult = getAverage(7,12); 
+console.log("the average is " + myResult);
