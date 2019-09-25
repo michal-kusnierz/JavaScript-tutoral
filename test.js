8fbc2617edc9f7d35c11d0b54df32bc9d047a1f4
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-25 13:14:23 +0200

    logical operator OR

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-var myAge = 39;
+var myAge = 25;
 
-if (myAge >= 18 && myAge <= 30){
+if (myAge < 18 || myAge > 30){
 
-    document.write("You can come, you cool dude");
+    document.write("you aint coming!");
 
 } else{
 
-    document.write("you aint coming!");
+    document.write("You can come, you cool dude");
 
 }
