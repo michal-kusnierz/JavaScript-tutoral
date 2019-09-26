7d6ceadce96ea014d839dbcc17f1207081d4ca88
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-26 12:57:11 +0200

    .toLowerCase

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
-var myString = 'I\'m a "fun ninja" string';
-if (myString.indexOf("ninja") === -1){
-	console.log("the word ninja is not in the string");
-} else{
-	console.log("the word ninja starts at position " + myString.indexOf("ninja"));
-}
+/* var string1 = "abc";
+var string2 = "ABC";
+
+console.log(string1 === string2);
+
+*/ 
+//the one above is going to return false. 
+
+var string1 = "abc";
+var string2 = "Abc";
+
+console.log(string1.toLowerCase() === string2.toLowerCase());
+
