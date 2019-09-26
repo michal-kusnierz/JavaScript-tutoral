9822b7cf8deec944b813bae5b3ac7dda0f5c9fd8
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-26 14:55:29 +0200

    Constructor Functions

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
-var myCar2 = {
-	maxSpeed: 70, 
-	driver: "Net Ninja", 
-	drive: function(speed, time){
-		console.log(speed * time);
-	},
-	logDriver: function(){
-		console.log("driver name is " + this.driver);
-	}
-};
+var Car = function(maxSpeed, driver){
 
-console.log(myCar2.maxSpeed);
-myCar2.drive(50,3);
-myCar2.logDriver();
+	this.maxSpeed = maxSpeed;	
+	this.driver = driver;
+	this.drive = function(speed, time){
+		console.log(speed * time);
+	};
+	this.logDriver = function(){
+		console.log("driver name is " + this.driver);
+	};
+}
+
+var myCar = new Car (40, "Ninja Man");
+var myCar2 = new Car (10, "Humpty Dumpty");
+var myCar3 = new Car (50, "Shaun");
+var myCar4 = new Car (100, "James Bond");
+
+myCar.drive(30,5);
+myCar3.logDriver();
