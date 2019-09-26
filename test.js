0765d6861831b9f541a56d6d1154ccd09171804c
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-26 15:03:15 +0200

    Date Object

--- test.js
+++ test.js
@@ -1,19 +1,8 @@
-var Car = function(maxSpeed, driver){
+var myDate = new Date();
+console.log(myDate);
 
-	this.maxSpeed = maxSpeed;	
-	this.driver = driver;
-	this.drive = function(speed, time){
-		console.log(speed * time);
-	};
-	this.logDriver = function(){
-		console.log("driver name is " + this.driver);
-	};
-}
+var myPastDate = new Date (1545,11,2, 10,30,15);
+var myFutureDate = new Date (2515,0,31,10,30,15);
 
-var myCar = new Car (40, "Ninja Man");
-var myCar2 = new Car (10, "Humpty Dumpty");
-var myCar3 = new Car (50, "Shaun");
-var myCar4 = new Car (100, "James Bond");
-
-myCar.drive(30,5);
-myCar3.logDriver();
+console.log(myPastDate);
+console.log(myFutureDate);
