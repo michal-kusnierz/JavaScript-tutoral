65d90027169de77a6ee2517270aa9ab929797904
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-26 15:14:03 +0200

    how to compare dates

--- test.js
+++ test.js
@@ -1,8 +1,28 @@
-var myDate = new Date();
-console.log(myDate);
+var birthday = new Date(1980,3,22, 11,15,25);
+var birthday2 = new Date(1980,3,22, 11,15,25);
 
-var myPastDate = new Date (1545,11,2, 10,30,15);
-var myFutureDate = new Date (2515,0,31,10,30,15);
 
-console.log(myPastDate);
-console.log(myFutureDate);
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+
+console.log(birthday.getTime());
+// get the number of milliseconds since 1st Jan 1970
+
+
+// to compare dates
+if(birthday == birthday2){
+	console.log("birthdays are equal");
+} else{
+	console.log("birthdays are not equal");
+}
+
+//why are they not equal? because they are different objects, but we can compare Time:
+
+if(birthday.getTime() == birthday2.getTime()){
+	console.log("birthdays are equal");
+} else{
+	console.log("birthdays are not equal");
+}
