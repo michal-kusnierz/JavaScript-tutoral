b50e520ea7fdffd7a6e836b87817b48f8d5ce01f
michal-kusnierz <kusnierz.michal@gmail.com>
2019-09-27 16:35:28 +0200

    jquery

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 var myPara = document.getElementById("content").getElementsByTagName("p")[5];
 var myPara2 = $("#content p:last-child");
 
-myPara2.addClass("test");
-myPara2.removeClass("test");
+// myPara2.addClass("test");
+// myPara2.removeClass("test");
 myPara2.fadeOut();
 myPara2.fadeIn();
 myPara2.css({position: "relative", color: "red"})
 myPara2.animate({left: "50px"});
+
+
+{/* <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> */}
+  
